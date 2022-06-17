99c87ee064dc75f2338450d0b3a31081018db543
Warwick <WarwickSeven@gmail.com>
2022-06-17 22:40:05 +0300

    Lesson E Task 1

--- WarwickHM_Diff_E/main.cpp
+++ WarwickHM_Diff_E/main.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <set>
 
+template<typename T> void DumbSearch(const T begin, const T end) {
+    std::cout << "Dumb search: "<< std::endl;
+    for (T p1=begin;p1!=end;++p1) {
+        T p2;
+        for (p2=begin; p2!=p1; ++p2)
+            if (*p1==*p2)
+                break;
+        if (p1==p2)
+            std::cout << *p1 << " ";
+    }
+    std::cout << std::endl;
+}
+
+template<typename T> void SmartSearch(const T begin, const T end) {
+    std::cout << "Smart search: "<< std::endl;
+    std::set<std::string> buf(begin, end);
+    for (auto i : buf) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+}
+
+
+int main() {
+    std::vector<std::string> MyVec = {"One", "Two", "Three", "Two", "Four", "Five", "One", "Two", "Snake!",};
+    std::multiset<std::string> MySet(MyVec.begin(), MyVec.end());
+    DumbSearch(MyVec.begin(), MyVec.end());
+    DumbSearch(MySet.begin(), MySet.end());
+    SmartSearch(MyVec.begin(),MyVec.end());
+    SmartSearch(MySet.begin(),MySet.end());
+    return 0;
+}

11425c04f223feebfc506e7484dd9ac5e837ac00
Warwick <WarwickSeven@gmail.com>
2022-06-17 21:35:51 +0300

    Initial Commit

---
+++ WarwickHM_Diff_E/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  WarwickHM_Diff_E
+//
+//  Created by Александр Тихонов on 17.06.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
